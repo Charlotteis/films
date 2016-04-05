12ede6b1a5f98e28209b71ae4a51f4b96a206608
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-04-05 11:15:56 +0100

    Add basic index page
    With a (probably overkill) test!

--- films/tests.py
+++ films/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class TestPages(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_page(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
