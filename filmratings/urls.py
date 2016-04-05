3e680de493800cac446ccc37ed4494310d7c8d6d
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-04-05 11:55:09 +0100

    Create about and contact pages

--- filmratings/urls.py
+++ filmratings/urls.py
@@ -19,5 +19,7 @@ from films import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^contact/$', views.contact, name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- films/tests.py
+++ films/tests.py
@@ -12,3 +12,15 @@ class TestPages(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_about_page(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_contact_page(self):
+        url = reverse('contact')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
