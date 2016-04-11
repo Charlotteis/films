dc949a4f4a3667b38aab1e4000c9911ea28cf0fd
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-04-11 09:29:45 +0100

    Add ability to change password once logged in

--- filmratings/urls.py
+++ filmratings/urls.py
@@ -17,7 +17,9 @@ from django.contrib.auth.views import (
     password_reset,
     password_reset_done,
     password_reset_confirm,
-    password_reset_complete
+    password_reset_complete,
+    password_change,
+    password_change_done,
 )
 from django.contrib import admin
 
@@ -43,6 +45,12 @@ urlpatterns = [
     url(r'^/accounts/password/done/$', password_reset_complete, {
         'template_name': 'registration/password_reset_complete.html'
     }, name='password_reset_complete'),
+    url(r'^accounts/password/change/$', password_change, {
+        'template_name': 'registration/password_change_form.html'
+    }, name='password_change'),
+    url(r'^/accounts/password/change/done$', password_change_done, {
+        'template_name': 'registration/password_change_done.html'
+    }, name='password_change_done'),
     url(r'^accounts/register/$', MyRegistrationView.as_view(),
         name='registration_register'),
     url(r'^accounts/create_film/$', views.create_film,
